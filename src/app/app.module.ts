0151be574e2e1b72d3c1be2ade51c726ac2fb6d3
radhilenny <45421640+radhilenny@users.noreply.github.com>
2018-12-18 10:24:01 +0100

    comit
    comit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+ 
+import {ROUTES} from './app.routes';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ArtistaComponent,
+    SearchComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES,{useHash:true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
